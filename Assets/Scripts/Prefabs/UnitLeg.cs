c7fda04bcf188ab6c9be5406ff4a03a1ecf38b8c
tjomagavk <tjomagavk@yandex.ru>
2021-06-15 23:50:48 +0300

    add unit + rotate legs

---
+++ Assets/Scripts/Prefabs/UnitLeg.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+namespace Prefabs
+{
+    public class UnitLeg : MonoBehaviour
+    {
+        public float currentX;
+        private bool _positive;
+        private bool _play = true;
+
+        private int _speed = 200;
+
+        public void SetPositive(bool positive)
+        {
+            _positive = positive;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            _speed = speed;
+            currentX = 0;
+        }
+
+        public void Stop()
+        {
+            _play = false;
+            currentX = 0;
+            rotate(currentX);
+        }
+
+        private void Update()
+        {
+            if (_play)
+            {
+                Steps();
+            }
+        }
+
+        private void Steps()
+        {
+            if (currentX >= 45)
+            {
+                _positive = false;
+            }
+
+            if (currentX <= -45)
+            {
+                _positive = true;
+            }
+
+            if (_positive)
+            {
+                currentX += Time.deltaTime * _speed;
+            }
+            else
+            {
+                currentX -= Time.deltaTime * _speed;
+            }
+
+            rotate(currentX);
+        }
+
+        private void rotate(float x)
+        {
+            Vector3 vector3 = new Vector3(x, 0, 0);
+            transform.localRotation = Quaternion.Euler(vector3);
+        }
+    }
+}
---
+++ Assets/Scripts/Prefabs/UnitScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Prefabs;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private UnitLeg leftLeg;
+    [SerializeField] private UnitLeg rightLeg;
+    private bool _leftLegPositive;
+    private float _currentLeftLegX = 45f;
+    public int speed;
+    public bool go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int legSpeed = speed * 100;
+        leftLeg.SetPositive(true);
+        leftLeg.SetSpeed(legSpeed);
+        rightLeg.SetPositive(false);
+        rightLeg.SetSpeed(legSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        // transform.Rotate(0, 0, 1);
+        if (go)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+        }
+    }
+}
